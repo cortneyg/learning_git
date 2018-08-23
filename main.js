ec0fe93fb702fd332fd04e9a0e72c457796155ed
Cortney Gardner <cortneygardner@PC-LM31571.overstock.com>
2018-08-22 19:33:19 -0600

    here you go

---
+++ main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded"), function(){
+  //variables 
+  var name = document.getElementById('name')
+  var startButton = document.getElementById('start-button')
+  name.addEventListener('keyup', function(event){
+    if (event.target.value !==''){
+      startButton.className=""
+      else{
+        startButton.className="hide"
+      }
+    
+    
+  })
+
+  //functions
+}
